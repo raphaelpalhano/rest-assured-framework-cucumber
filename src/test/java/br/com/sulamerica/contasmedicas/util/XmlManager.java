eabe174ced2ff2d1b3c3c14ec6a15976e56e7415
raphael angel <raphael1angel@hotmail.com>
2023-10-29 14:02:14 -0300

    create xml manager util

---
+++ src/test/java/br/com/sulamerica/contasmedicas/util/XmlManager.java
@@ -0,0 +1,62 @@
+package br.com.sulamerica.contasmedicas.util;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Map;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+public class XmlManager {
+    
+    /*
+     * Exemplo de update
+     * 
+     * Map<String, String> mod = new HashMap<String, String>();
+     *   mod.put("indRetificacao", "certo");
+     *  mod.put("tpAmb", "certo");
+     *   mod.put("aplicEmi", "certo");
+     *   updateXml(mod, "cadastro_intermediario.xml");
+     * 
+     * 
+     */
+      public void updateXml(Map<String, String> valoresParaModificarXml, String nomeXml) throws Exception {
+        InputStream inputStream = new FileInputStream(new File(System.getProperty("user.dir") + File.separator + nomeXml));
+        DocumentBuilderFactory domFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder domBuilder = domFactory.newDocumentBuilder();
+        Document document = domBuilder.parse(inputStream);
+
+        for (Map.Entry<String, String> entry : valoresParaModificarXml.entrySet()) {
+           NodeList rowNodes = document.getElementsByTagName(entry.getKey());
+            for (int i = 0; i < rowNodes.getLength(); i++) {
+                Node rowNode = rowNodes.item(i);
+
+                // Remove existing content (if any)
+                while (rowNode.getFirstChild() != null)
+                    rowNode.removeChild(rowNode.getFirstChild());
+
+                // Add text content
+                rowNode.appendChild(document.createTextNode(entry.getValue()));
+            }
+        }
+            
+        // Save XML to file
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+        transformer.transform(new DOMSource(document),
+                new StreamResult(System.getProperty("user.dir") + File.separator + nomeXml));
+
+    }
+
+}
